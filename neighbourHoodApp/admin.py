abfb40ba8f22cd8268bf35e3153b2852bc43010a
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-04-18 13:05:15 +0300

    add the admin registration  file

---
+++ neighbourHoodApp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Profile,NeighbourHood,Bussiness,Post
+# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(NeighbourHood)
+admin.site.register(Bussiness)
+admin.site.register(Post)
