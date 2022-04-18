59c776782dab680a2ff544cf7da862451c023fcb
sylvestus <sylvestus.sigei@student.moringaschool.com>
2022-04-18 14:28:11 +0300

    heroku deployments

--- neighbourHood/settings.py
+++ neighbourHood/settings.py
@@ -45,6 +45,11 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 MODE=config("MODE", default="dev")
 
 # ALLOWED_HOSTS = []
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 # Application definition
